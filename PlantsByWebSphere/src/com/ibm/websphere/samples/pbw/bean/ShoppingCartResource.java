4760ef42f27ad050f6e42d881dcfa6bc0aa594ee
sgivanec <sgivanec@us.ibm.com>
2017-02-14 12:36:04 -0600

    Final change which brings us back to full working version of the rest branch.

--- PlantsByWebSphere/src/com/ibm/websphere/samples/pbw/bean/ShoppingCartResource.java
+++ PlantsByWebSphere/src/com/ibm/websphere/samples/pbw/bean/ShoppingCartResource.java
@@ -1,16 +1,30 @@
 package com.ibm.websphere.samples.pbw.bean;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.ibm.websphere.samples.pbw.jpa.Inventory;
 
 @Path("/cart")
 public class ShoppingCartResource {
 
+	@Inject
+	private ShoppingCartBean cart;
+	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public int getShoppingCartSize() {
-		return 5; // needs to be changed to get actual number from shopping cart bean
+		return cart.getSize(); // needs to be changed to get actual number from shopping cart bean
+	}
+	
+	@POST
+	@Path("{id}")
+	public void addItemToCart(@PathParam("id") String id) {
+		cart.addItem(new Inventory(id.toString(), "Bonsai Tree", "This is Sam's Bonsai Tree", "Description", "PackageInfo", "imageHere", 30.0f, 30.0f, 100, 1, "Nothing for notes", true));
 	}
 	
 }
